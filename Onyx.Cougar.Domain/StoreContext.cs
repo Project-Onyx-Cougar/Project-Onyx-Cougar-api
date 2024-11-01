d63bb8ea29efeaf546fd036816eff9b69fc6bdab
Michelle Alaniz <alaniz.15@osu.edu>
2024-10-31 21:11:20 -0400

    Part 2 Data Access: Create Database Context

---
+++ Onyx.Cougar.Domain/StoreContext.cs
@@ -0,0 +1,14 @@
+using Onyx.Cougar.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Onyx.Cougar.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+            { }
+
+            public DbSet<Item> Items { get; set; }
+    }
+}
